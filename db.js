fb396902cd9a10cf3dbb0ebde33996619333ba21
Rohit <rohitkalase444@gmail.com>
2022-07-14 13:52:43 +0530

    mern app

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose =require('mongoose');
-const mongoURL="mongodb://localhost:27017/rnotebook"
+const mongoURL="mongodb+srv://rohit29:rohit@29@cluster0.k2zjz.mongodb.net/rnotebook?retryWrites=true&w=majority"
 const connectToMongo=()=>{
     mongoose.connect(mongoURL , ()=>{
         console.log("connected!")
